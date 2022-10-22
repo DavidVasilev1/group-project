601da6e9a0746030724da9e8ae7bcbcb915123a4
DavidVasilev1 <DavidVasilev1@users.noreply.github.com>
2022-10-22 19:38:27 +0000

    deploy: 8364e149cd4c25fc3838eb247459c84573ff1c79

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/Ivy-League-Calculator/assets/js/search-data.json', true);
+    request.open('GET', '/group-project/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
